a67907dc77883b1067101febd4ed22f6a0ce99e7
Andrew Z <azhong2002@outlook.com>
2021-02-23 22:18:13 -0500

    Implement historical data retrieval

--- simulation.py
+++ simulation.py
@@ -1,19 +1,41 @@
 import yfinance as yf
+import pandas as pd
 
 def get_last_price(ticker):
     return yf.Ticker(ticker).history().tail(1)['Close'].iloc[0]
 
+def get_price_at(ticker, date):
+    return yf.Ticker(ticker).history(date)['Open']
+
+# start and end in "YEAR-MO-DA" format, with 0s in front
+def get_historical_data(tickers, start, end):
+    data = yf.download(tickers, start=start,
+                       end=end, group_by='tickers')
+
+    series = {ticker : data[ticker]['Open'] for ticker in tickers}
+
 class Simulation:
 
-    def __init__(this, portfolio):
+    def __init__(this, portfolio, date=None):
         this.portfolio = portfolio
-        this.deposit = this.calc_value()
+        if date:
+            this.date = date
+        else:
+            today = pd.Timestamp.today()
+            this.date = '%i-%i-%i' % (today.year, today.month, today.day)
+        this.deposit = this.value()
 
-    def calc_value(this):
+    def value(this):
+        return sum([shares * get_price_at(ticker, this.date) for ticker, shares in this.portfolio.items()])
+
+    def gains(this):
+        return this.deposit - this.value()
+
+    def present_value(this):
         return sum([shares * get_last_price(ticker) for ticker, shares in this.portfolio.items()])
 
-    def calc_gains(this):
-        return this.deposit - this.calc_value()
+    def present_gains(this):
+        return this.deposit - this.present_value()
 
     def buy_stocks(this, stocks):
         for ticker, shares in stocks.items():
